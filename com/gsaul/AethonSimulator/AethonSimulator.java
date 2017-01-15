20289781ba566e37f14d77ba873ff7f8bbad9acc
Gavin Saul <chemist931@gmail.com>
2017-01-15 15:04:16 -0500

    Set framework for updateVars()

--- com/gsaul/AethonSimulator/AethonSimulator.java
+++ com/gsaul/AethonSimulator/AethonSimulator.java
@@ -4,24 +4,48 @@ import java.util.Date;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.image.BufferedImage;
 
 public class AethonSimulator
 {
+   static ControlPane control = new ControlPane();
    public static void main(String[] args)
    {
-      final ScheduleExecutorService advancer = Executors.newSingleThreadScheduledExecutor();
+      JFrame frame = new JFrame("Aethon Control Panel");
+      frame.setSize(1366, 768);
+      frame.setLocation(0, 0);
+      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      frame.setContentPane(control);
+      frame.setResizable(false);
+      frame.setVisible(true);
+      
+      final ScheduledExecutorService advancer = Executors.newSingleThreadScheduledExecutor();
       advancer.scheduleWithFixedDelay(new Runnable()
          {
             @Override
             public void run()
             {
-               update();
+               updateVars();
             }
          }, 0, 1, TimeUnit.SECONDS);   
    }
    
-   public void update()
+   public static void updateVars()
    {
-      
+      control.updateVars();
+   }
+}
+
+class ControlPane extends JPanel
+{
+   public ControlPane()
+   {
+   }
+   
+   public BufferedImage updateVars()
+   {
+      return new BufferedImage(1, 1, 1);
    }
 }
