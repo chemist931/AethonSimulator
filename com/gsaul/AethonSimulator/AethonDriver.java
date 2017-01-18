9576eaba5bda2e728c4a6020595b18e3e9c85073
Gavin Saul <chemist931@gmail.com>
2017-01-18 09:12:54 -0500

    Reset com/ dir

---
+++ com/gsaul/AethonSimulator/AethonDriver.java
@@ -0,0 +1,31 @@
+package com.gsaul.AethonSimulator;
+
+import java.util.concurrent.*;
+import javax.swing.*;
+
+public class AethonDriver {
+    static ControlPane control = new ControlPane();
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Aethon Control Panel");
+        frame.setSize(1366, 768);
+        frame.setLocation(0, 0);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setContentPane(control);
+        frame.setResizable(false);
+        frame.setVisible(true);
+
+        final ScheduledExecutorService advancer = Executors.newSingleThreadScheduledExecutor();
+        advancer.scheduleWithFixedDelay(new Runnable() {
+            @Override
+            public void run() {
+                update();
+            }
+        }, 0, 1, TimeUnit.SECONDS);
+    }
+
+    private static void update() {
+        control.updateVars();
+    }
+}
+
